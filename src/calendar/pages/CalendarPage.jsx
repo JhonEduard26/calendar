0915c5bbe3fcb5c9d21f48f32ab77b009aa87cf0
Jhon Eduard Bocanegra <jhon.bocanegra26@gmail.com>
2022-12-26 21:06:36 -0500

    [ADD] calendar and calendar event box

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,35 @@
-import { Navbar } from '../components'
+import { Calendar } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { addHours } from 'date-fns'
+
+import { CalendarEvent, Navbar } from '../components'
+import { localizer } from '../../helpers'
+
+const events = [
+  {
+    title: 'Cumpleaños',
+    note: 'Comprar el pastel',
+    start: new Date(),
+    end: addHours(new Date(), 1),
+    user: 'Jhon'
+  }
+]
 
 export const CalendarPage = () => {
   return (
     <>
     <Navbar />
-    <div>CalendarPage</div>
+
+    <Calendar
+      localizer={localizer}
+      events={events}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+      components={{
+        event: CalendarEvent
+      }}
+    />
     </>
   )
 }
