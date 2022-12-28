01db302c39d547b639aa0b29fc3a818fb6a3b298
Jhon Eduard Bocanegra <jhon.bocanegra26@gmail.com>
2022-12-27 19:17:58 -0500

    [ADD] modal content and date picker

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
+import { addHours } from 'date-fns'
 import Modal from 'react-modal'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+
 import './components.css'
 
 Modal.setAppElement('#root')
@@ -9,16 +13,41 @@ export const CalendarModal = () => {
   let subtitle
   const [modalIsOpen, setIsOpen] = useState(true)
 
-  function openModal() {
+  const onInputChange = ({ target }) => {
+
+    const { value, name } = target
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  const [formValues, setFormValues] = useState({
+    title: 'Jhon',
+    notes: 'Bocanegra',
+    start: new Date(),
+    end: addHours(new Date(), 2)
+  })
+
+  const { title, notes, start, end } = formValues
+
+  const onDateChanged = (event, changing) => {
+    setFormValues({
+      ...formValues,
+      [changing]: event
+    })
+  }
+
+  const openModal = () => {
     setIsOpen(true)
   }
 
-  function afterOpenModal() {
+  const afterOpenModal = () => {
     // references are now sync'd and can be accessed.
     subtitle.style.color = '#f00'
   }
 
-  function onCloseModal() {
+  const onCloseModal = () => {
     setIsOpen(false)
   }
 
@@ -30,9 +59,59 @@ export const CalendarModal = () => {
       overlayClassName="modal-fondo"
       closeTimeoutMS={200}
     >
-      <h2>Hello</h2>
-      <button onClick={onCloseModal}>close</button>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae unde dolores voluptates voluptatem ipsum iure libero, porro eligendi itaque cum sapiente. Voluptate, ex incidunt in quas tempora tenetur numquam tempore?</p>
+      <h1> New Event </h1>
+      <hr />
+      <form className='modal__form-content'>
+
+        <label htmlFor='startdate'>Date and time start</label>
+        <DatePicker
+          selected={start}
+          onChange={(event) => onDateChanged(event, 'start')}
+          dateFormat="Pp"
+          showTimeSelect
+        />
+
+        <label htmlFor='enddate'>Date and time end</label>
+        <DatePicker
+          minDate={start}
+          selected={end}
+          onChange={(event) => onDateChanged(event, 'end')}
+          dateFormat="Pp"
+          showTimeSelect
+        />
+
+        <hr />
+        <label htmlFor='title'>Title and notes</label>
+        <input
+          type="text"
+          placeholder="Event title"
+          name="title"
+          value={title}
+          onChange={onInputChange}
+          autoComplete="off"
+          id='title'
+        />
+        <small id="emailHelp">Small description</small>
+
+        <textarea
+          type="text"
+          placeholder="Notes"
+          rows="5"
+          name="notes"
+          value={notes}
+          onChange={onInputChange}
+        ></textarea>
+        <small id="emailHelp">Extra information</small>
+
+        <button
+          className='save--button'
+          type="submit"
+        >
+          <i className="far fa-save"></i>
+          <span>  Save</span>
+        </button>
+
+      </form>
     </Modal>
   )
 }
